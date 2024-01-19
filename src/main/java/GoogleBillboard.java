3c0061d10eca67f70c362f90ee00e83f7f6f4d16
chyeung1 <143123817+chyeung1@users.noreply.github.com>
2024-01-18 21:05:27 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,3 +1,4 @@
+public class GoogleBillboard {
 public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 public void setup()  
 {             
@@ -17,5 +18,6 @@ public boolean isPrime(double dNum)
     if(dNum % i == 0)
     return false;
   } 
-    return true;  
-} 
+   return true;  
+ } 
+}

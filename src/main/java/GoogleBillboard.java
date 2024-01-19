f1be001d8566af25cb50fe4ac8de415d4c324f71
chyeung1 <143123817+chyeung1@users.noreply.github.com>
2024-01-18 21:08:23 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,13 +1,15 @@
-public class GoogleBillboard {
+public double GoogleBillboard {
 public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public double answer= 0;
 public void setup()  
 {             
  for(int i = 2; i < e.length() - 10; i++) {
   if(isPrime(Double.parseDouble(e.substring(i, i + 10)))) {   
-    System.out.println(Double.parseDouble(e.substring(i, i + 10)));
+    answer = Double.parseDouble(e.substring(i, i + 10));
     break;
   }
  } 
+ return answer;
 }
 public boolean isPrime(double dNum)  
 {   
